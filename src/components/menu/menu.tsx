4d1cb96b5b1cb087044f709a77f228cf3d3362eb
Gintare <gintare@Gintares-MacBook-Pro.local>
2025-01-11 18:34:40 +0200

    fix: Menu component structure

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -2,23 +2,23 @@ import { NavLink } from 'react-router-dom';
 import styles from './menu.module.css';
 import clsx from 'clsx';
 
+const links = [
+  { name: 'Home', link: '/', id: 1 },
+  { name: 'Task description', link: '/task', id: 2 },
+];
+
 export const Menu = () => {
   return (
     <menu className={styles.container}>
-      <li>
-        <NavLink
-          to="/"
-          className={({ isActive }) => clsx(isActive && styles.active)}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/task"
-          className={({ isActive }) => clsx(isActive && styles.active)}>
-          Task description
-        </NavLink>
-      </li>
+      {links.map(({ name, link, id }) => (
+        <li key={id}>
+          <NavLink
+            to={link}
+            className={({ isActive }) => clsx(isActive && styles.active)}>
+            {name}
+          </NavLink>
+        </li>
+      ))}
     </menu>
   );
 };
